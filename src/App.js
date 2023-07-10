1f4a3e153a70f601ea69f44cc1fa1e8bde13db14
Elu620758 <elumalai620758@gmail.com>
2023-07-11 00:42:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import NavBar from './component/NavBar';
+import CyberSecurity from './component/Cybersecurity';
+import FullStackDevelopment from './component/FullStack';
+import Home from './component/Home';
+import UIUX from './component/UIUX';
+import DataScience from './component/DataScience';
+import Footer from './component/footer';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <header className="header">
+        <div className='header-top'>
+          <img id='img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStmiwZZZUl4BOL9FSLelRPx-4dRubjmIHJAg&usqp=CAU" alt="IT Course Blog" />
+          <h1 className='header'>Best IT Course Blog</h1>
+          <h2 className="highlight">By Elumalai</h2>
+        </div>
+      </header>  
+      <NavBar />   
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/FullStackDevelopment' element={<FullStackDevelopment />} />
+        <Route path='/CyberSecurity' element={<CyberSecurity />} />
+        <Route path='/DataScience' element={<DataScience />} />
+        <Route path='/UIUX' element={<UIUX />} />
+      </Routes>
+      <Footer />
     </div>
+
   );
 }
 
